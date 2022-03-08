b8bceeea4750045ccb3d6afc58297ebc1d6924e5
Davi3684 <davidhulstroem@gmail.com>
2022-03-08 12:14:36 +0100

    Revert "kuhhjk"
    This reverts commit 2ffd6d43d4ce8f550c3980629f333b79eda043a7, reversing
    changes made to ec27170023ee17c70c272aea2ed243767793b28d.

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,31 +2,14 @@ extends KinematicBody2D
 
 # 
 const ACCELERATION = 800
-const MAX_SPEED = 180
+const MAX_SPEED = 300
 const FRICTION = 1200
-const PROJECTILE_SPEED = 200
+
 #
 var velocity = Vector2.ZERO
-var projectile = preload("res://Player/Projectile.tscn")
 
 
 func _physics_process(delta):
-	MovePlayer(delta)
-	Shoot()
-
-func Shoot():
-	var shoot_vector = Vector2.ZERO
-	shoot_vector.x = Input.get_action_strength("shoot_right") - Input.get_action_strength("shoot_left")
-	shoot_vector.y = Input.get_action_strength("shoot_down") - Input.get_action_strength("shoot_up")
-	
-	shoot_vector = shoot_vector.normalized() * PROJECTILE_SPEED
-	
-	if shoot_vector != Vector2.ZERO:
-		var p = projectile.instance()
-		p.shoot(position, shoot_vector + velocity)
-		get_parent().add_child(p)
-
-func MovePlayer(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -39,3 +22,4 @@ func MovePlayer(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+
